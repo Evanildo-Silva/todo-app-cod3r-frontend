9e869a3dd34568b50b6b11299abf37e34fc83b4b
Evanildo Silva <evanildo.sf91@gmail.com>
2023-07-19 19:52:07 -0300

    feat: component navbar

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -2,9 +2,10 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default props => (
   <div className='container'>
-    <h1>Teste</h1>
+    <Navbar />
   </div>
 )
