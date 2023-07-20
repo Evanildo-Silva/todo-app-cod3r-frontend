0f9b26f725b421298436038e3ceb807011fe4d7a
Evanildo Silva <evanildo.sf91@gmail.com>
2023-07-19 23:08:36 -0300

    feat: add events onclick and onchange

--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -3,11 +3,31 @@ import PageHeader from '../components/PageHeader'
 import TodoForm from '../components/TodoForm'
 
 export default class Todo extends Component {
+  // "Amarrando" o this ao Todo component
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+  handleAdd() {
+
+  }
+
   render() {
     return (
       <div>
-        <PageHeader name='Tarefas' small='Cadastro'/>
-        <TodoForm />
+        <PageHeader name='Tarefas' small='Cadastro' />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
       </div>
     )
   }
